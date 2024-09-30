259701247ade0117d0f27321346b8da9271e6f88
ashutoshkumarjha500 <ashutoshkumarjha500@gmail.com>
2024-09-30 13:14:40 +0530

    n

--- src/Component/layout/footer.jsx
+++ src/Component/layout/footer.jsx
@@ -2,7 +2,6 @@ import { Copyright } from 'lucide-react';
 
 // import Typography from './Component/general/typography';
 import Typography from '../general/typography';
-// import Link from '../Navigation/link';
 import Link from '../navigation/link';
 import { EXTERNAL_LINKS } from '../lib/data';
 
