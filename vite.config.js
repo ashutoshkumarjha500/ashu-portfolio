8c2f2bfc91c8e473f99c501130c64bcd9926d5dc
ashutoshkumarjha500 <ashutoshkumarjha500@gmail.com>
2024-09-30 13:48:27 +0530

    v

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['clsx'], // Mark 'clsx' as an external dependency
+    },
+  },
+});
 
-// vite.config.js
-// export default {
-//   build: {
-//     rollupOptions: {
-//       external: ['clsx'],
-//     },
-//   },
-// };
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+
